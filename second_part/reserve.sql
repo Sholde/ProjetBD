3b14d5f064a7ef0a413df44abe9a559372dbeb42
Nicolas Bouton <nicolas.bouton@ens.uvsq.fr>
2019-11-24 16:00:57 +0100

    correction de coquille, comme date -> jour, VERSION -> version, numero -> num, pour que ca soit uniforme

--- second_part/reserve.sql
+++ second_part/reserve.sql
@@ -1,20 +1,20 @@
 /* Table */
 CREATE TABLE Clients(
-    numero_client INT(4),
+    num_client INT,
     nom VARCHAR(30) NOT NULL,
     prenom VARCHAR(30) NOT NULL,
     email VARCHAR(30) NOT NULL UNIQUE,
     mot_de_passe VARCHAR(30) NOT NULL,
     type_de_reduction VARCHAR(30) NOT NULL,
-    PRIMARY KEY(numero_client)
+    PRIMARY KEY(num_client)
 );
 CREATE TABLE Film(
-    numero_film INT(3),
+    num_film INT(3),
     nom VARCHAR(30) NOT NULL,
     genre VARCHAR(30) NOT NULL,
     duree INT NOT NULL,
     origine VARCHAR(30) NOT NULL,
-    PRIMARY KEY(numero_film)
+    PRIMARY KEY(num_film)
 );
 CREATE TABLE Cinema(
     nom VARCHAR(30),
@@ -22,79 +22,79 @@ CREATE TABLE Cinema(
     PRIMARY KEY(nom)
 );
 CREATE TABLE Salle(
-    numero_salle INT,
+    num_salle INT,
     nom_du_cinema VARCHAR(30),
     nombre_de_place INT(3) NOT NULL,
     ville VARCHAR(30) NOT NULL,
-    PRIMARY KEY(numero_salle, nom_du_cinema),
+    PRIMARY KEY(num_salle, nom_du_cinema),
     FOREIGN KEY(nom_du_cinema) REFERENCES Cinema(nom)
 );
 CREATE TABLE Veut_voir(
-    numero_client INT,
-    numero_film INT,
+    num_client INT,
+    num_film INT,
     prix INT NOT NULL,
-    PRIMARY KEY(numero_client, numero_film),
-    FOREIGN KEY(numero_client) REFERENCES Clients(numero_client),
-    FOREIGN KEY(numero_film) REFERENCES Film(numero_film)
+    PRIMARY KEY(num_client, num_film),
+    FOREIGN KEY(num_client) REFERENCES Clients(num_client),
+    FOREIGN KEY(num_film) REFERENCES Film(num_film)
 );
 CREATE TABLE Note(
-    numero_client INT,
+    num_client INT,
     numero_film INT,
     note INT NOT NULL,
-    PRIMARY KEY(numero_client, numero_film),
-    FOREIGN KEY(numero_client) REFERENCES Clients(numero_client),
-    FOREIGN KEY(numero_film) REFERENCES Film(numero_film)
+    PRIMARY KEY(num_client, num_film),
+    FOREIGN KEY(num_client) REFERENCES Clients(num_client),
+    FOREIGN KEY(num_film) REFERENCES Film(num_film)
 );
 CREATE TABLE Suit(
-    numero_film_prec INT,
-    numero_film_suiv INT,
+    num_film_prec INT,
+    num_film_suiv INT,
     PRIMARY KEY(
-        numero_film_prec,
-        numero_film_suiv
+        num_film_prec,
+        num_film_suiv
     ),
-    FOREIGN KEY(numero_film_prec) REFERENCES Film(numero_film),
-    FOREIGN KEY(numero_film_suiv) REFERENCES Film(numero_film)
+    FOREIGN KEY(num_film_prec) REFERENCES Film(num_film),
+    FOREIGN KEY(num_film_suiv) REFERENCES Film(num_film)
 );
 CREATE TABLE Se_joue_dans(
-    DATE INT,
+    jour INT,
     heure INT,
-    VERSION VARCHAR(30) NOT NULL,
-    numero_film INT,
+    version VARCHAR(30) NOT NULL,
+    num_film INT,
     num_salle INT,
     PRIMARY KEY(
-        numero_film,
+        num_film,
         num_salle,
-        DATE,
+        jour,
         heure
     ),
-    FOREIGN KEY(numero_film) REFERENCES Film(numero_film),
-    FOREIGN KEY(num_salle) REFERENCES Salle(numero_salle)
+    FOREIGN KEY(num_film) REFERENCES Film(num_film),
+    FOREIGN KEY(num_salle) REFERENCES Salle(num_salle)
 );
 CREATE TABLE Personne(
-    numero_personne INT,
+    num_personne INT,
     nom VARCHAR(30) NOT NULL,
     prenom VARCHAR(30) NOT NULL,
     age INT NOT NULL,
     metier VARCHAR(30) NOT NULL,
-    PRIMARY KEY(numero_personne)
+    PRIMARY KEY(num_personne)
 );
 CREATE TABLE Participe_au_film(
-    numero_personne INT,
-    numero_film INT,
-    PRIMARY KEY(numero_personne, numero_film),
-    FOREIGN KEY(numero_personne) REFERENCES Personne(numero_personne),
-    FOREIGN KEY(numero_film) REFERENCES Film(numero_film)
+    num_personne INT,
+    num_film INT,
+    PRIMARY KEY(num_personne, num_film),
+    FOREIGN KEY(num_personne) REFERENCES Personne(num_personne),
+    FOREIGN KEY(num_film) REFERENCES Film(num_film)
 );
 
 /* Vue */
 create view film_francais
 from film f, se_joue_dans j
-where f.numero_film = j.numero_film
+where f.num_film = j.num_film
 and j.version like "vf";
 
 create view film_vo
 from film f, se_joue_dans j
-where f.numero_film = j.numero_film
+where f.num_film = j.num_film
 and j.version like "vo";
 
 create view personne_majeur
