cafaf32a01e5022247bd686940d339fee56e16e1
Sholde <exshode@gmail.com>
2019-11-24 18:40:21 +0100

    ajout de deux requete update

--- second_part/reserve.sql
+++ second_part/reserve.sql
@@ -305,3 +305,17 @@ from Film f, Veut_voir v
 where f.num_film = v.num_film
 group by f.nom
 having sum(v.num_client) > 1000
+
+/* Requête update */
+
+/* Bob change sa note sur le film The Matrix */
+update Note
+set note = 5
+where n.num_client = 1 /* num_client de bob */
+and n.num_film = 1
+
+/* un admin change la salle du film The Matrix dans la salle 4 au cinéma Pathé Boulogne*/
+update Se_joue_dans
+set num_salle = 4
+where num_film = 1
+and nom_cinema = "Pathé Boulogne"
