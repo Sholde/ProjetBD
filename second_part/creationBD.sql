5fefa36ce0028761dadbe66fe4bc2a345ed53867
Sholde <exshode@gmail.com>
2019-11-25 19:25:16 +0100

    correction droit utilisateur

--- second_part/creationBD.sql
+++ second_part/creationBD.sql
@@ -9,6 +9,10 @@ create user 'Anonyme'@'localhost' identified by 'anonyme';
 grant all on Projet.* to 'Admin'@'localhost';
 
 grant select on Projet.* to 'Anonyme'@'localhost';
+grant select on Projet.Clients to 'Anonyme'@'localhost';
+grant select on Projet.VeutVoir to 'Anonyme'@'localhost';
+grant select on Projet.Note to 'Anonyme'@'localhost';
+grant select on Projet.Ticket from 'Anonyme'@'localhost';
 revoke select on Projet.Clients from 'Anonyme'@'localhost';
 revoke select on Projet.VeutVoir from 'Anonyme'@'localhost';
 revoke select on Projet.Note from 'Anonyme'@'localhost';
@@ -18,3 +22,4 @@ grant select on Projet.* to 'Client'@'localhost';
 grant all on Projet.Clients to 'Client'@'localhost';
 grant all on Projet.VeutVoir to 'Client'@'localhost';
 grant all on Projet.Note to 'Client'@'localhost';
+grant all on Projet.Ticket from 'Client'@'localhost';
