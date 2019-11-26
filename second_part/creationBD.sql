e85e35702d96c3ae6195c8329bb09475a799b236
Sholde <exshode@gmail.com>
2019-11-26 22:02:04 +0100

    correction creationBD, mtn les utilisateur c'est règlé

--- second_part/creationBD.sql
+++ second_part/creationBD.sql
@@ -2,24 +2,27 @@
 create database Projet;
 use Projet;
 
-create user 'Client'@'localhost' identified by 'client';
 create user 'Admin'@'localhost' identified by 'admin';
 create user 'Anonyme'@'localhost' identified by 'anonyme';
+create user 'Client'@'localhost' identified by 'client';
 
 grant all on Projet.* to 'Admin'@'localhost';
 
-grant select on Projet.* to 'Anonyme'@'localhost';
-grant select on Projet.Clients to 'Anonyme'@'localhost';
-grant select on Projet.VeutVoir to 'Anonyme'@'localhost';
-grant select on Projet.Note to 'Anonyme'@'localhost';
-grant select on Projet.Ticket from 'Anonyme'@'localhost';
-revoke select on Projet.Clients from 'Anonyme'@'localhost';
-revoke select on Projet.VeutVoir from 'Anonyme'@'localhost';
-revoke select on Projet.Note from 'Anonyme'@'localhost';
-revoke select on Projet.Ticket from 'Anonyme'@'localhost';
+grant select on Film to 'Anonyme'@'localhost';
+grant select on Personne to 'Anonyme'@'localhost';
+grant select on Participe_au_film to 'Anonyme'@'localhost';
+grant select on Cinema to 'Anonyme'@'localhost';
+grant select on Se_joue_dans to 'Anonyme'@'localhost';
+grant select on Salle to 'Anonyme'@'localhost';
+grant select on Suit to 'Anonyme'@'localhost';
 
-grant select on Projet.* to 'Client'@'localhost';
-grant all on Projet.Clients to 'Client'@'localhost';
-grant all on Projet.VeutVoir to 'Client'@'localhost';
-grant all on Projet.Note to 'Client'@'localhost';
-grant all on Projet.Ticket from 'Client'@'localhost';
+grant select on Film to 'Client'@'localhost';
+grant select on Personne to 'Client'@'localhost';
+grant select on Participe_au_film to 'Client'@'localhost';
+grant select on Cinema to 'Client'@'localhost';
+grant select on Se_joue_dans to 'Client'@'localhost';
+grant select on Salle to 'Client'@'localhost';
+grant select on Suit to 'Client'@'localhost';
+grant all on Clients to 'Client'@'localhost';
+grant all on Veut_voir to 'Client'@'localhost';
+grant all on Note to 'Client'@'localhost';
