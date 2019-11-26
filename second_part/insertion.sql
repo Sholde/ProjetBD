834fd90fbdd416aa307bf4324779693793629fb5
dytq <43381918+dytq@users.noreply.github.com>
2019-11-26 22:13:52 +0100

    Ajout de clients

--- second_part/insertion.sql
+++ second_part/insertion.sql
@@ -51,6 +51,126 @@ insert into Clients values (47, "Ionesco", "Eugène", "iones.eug@email.com", "eu
 insert into Clients values (48, "Ali", "Mohamed", "momoleboxeur@email.com", "box", 0);
 insert into Clients values (49, "Lavoisier", "Antoine Laurent", "antoinelavoisier@email.com", "ant", 0);
 insert into Clients values (50, "Mandela", "Nelson", "mand.nels@email.com", "nels", 0);
+insert into Clients values (51, "Kepler","Johannes","Johannes.Kepl@email.com","kel",0);
+insert into Clients values (52, "Descartes","René","Descartes.Rene@email.com","ren",1);
+insert into Clients values (53, "Torricelli","Evangelista","evang.torr@email.com","eve",0); 
+insert into Clients values (54, "Newton","Isaac","iss.newt@email.com","new",0); 
+insert into Clients values (55, "Watt","James","watt.james@email.com","watt",0);
+insert into Clients values (56, "Becquerel","Henri","henri.beck@email.com","becq",1);
+insert into Clients values (57, "Lorentz","Hendrik","lorentz.hendrik@email.com","lo",1);
+insert into Clients values (58, "Curie","Pierre","Pierre.curie@email.com","cur",0);
+insert into Clients values (59, "Bourgeois","Léon","bourg.leon@email.com","leon",0);                 
+insert into Clients values (60, "Simon","Claude","simon.claude@email.com","claude",1);                       
+insert into Clients values (61, "Allais","Maurice","allais.maurice@email.com","ail",1);                                               
+insert into Clients values (62, "Debreu","Gérard","debreu.gérard@email.com","gege",0);                       
+insert into Clients values (63, "Cassin","René","cassin.rene@email.com","rene",0);                       
+insert into Clients values (64, "Néel","Louis","louis.neel@email.com","louis",0);                       
+insert into Clients values (65, "Jacob","François","jacob.franc@email.com","franc",0);                       
+insert into Clients values (66, "Lwoff","André","andre.lwoff@email.com","lw",0);                       
+insert into Clients values (67, "Sartre","Jean-Paul","jean.sartre@email.com","jp",0);
+insert into Clients values (68, "Camus","Albert","albert.camus@email.com","al",1);
+insert into Clients values (69, "Mauriac","François","mauriac.francois@email.com","fanc",0);
+insert into Clients values (70, "Jouhaux","Léon","leon.jouhaux@email.com","leon",0);
+insert into Clients values (71, "Sauvage","Jean-Pierre","sauvage.jp@email.com","jp",1);
+insert into Clients values (72, "Tirole","Jean","jean.tirole@emaiil.com","tirol",0);
+insert into Clients values (73, "Modiano","Patrick","modiano.patrick@email.com","modiano",0);
+insert into Clients values (74, "Serge","Haroche","serge.haroche@email.com","serg",0);
+insert into Clients values (75, "Chauvin","Yves","yves.chauvin@email.com","yves",1);
+insert into Clients values (76, "Xingjian","Gao","xingjian.gao@email.com","gao",0);
+insert into Clients values (77, "Agre","Peter","peter.agre@email.com","agre",0);
+insert into Clients values (78, "Alder","Kurt","alder.kurt@email.com","kurt",1);
+insert into Clients values (79, "Altman","Sidney","atlman.sydney@email.com","sid",0);
+insert into Clients values (80, "Sanger","Aziz","aziz.sanger@email.com","aziz",0);
+insert into Clients values (81, "Shirakawa","Hideki","hideki.shira@email.com","hid",0);
+insert into Clients values (82, "Steitz","Thomas","steitz.thomas@email.com","thomas",0);
+insert into Clients values (83, "Yonath","Ada","ada.yonath@email.com","yonath",0);
+insert into Clients values (84, "Yoshino","Akira","akira.yoshino@email.com","akira",1);
+insert into Clients values (85, "Wittig","Georg","wittig.georg@email.com","georg",0);
+insert into Clients values (86, "Wallach","Otto","wallach.otto@email.com","otto",0);
+insert into Clients values (87, "Warshel","Arieh","warshel.arieh@email.com","ariech",0);
+insert into Clients values (88, "Tsien","Roger","tsien.roger@email.com","roger",0);
+insert into Clients values (89, "Taube","Henry","henry.taube@email.com","taube",0);
+insert into Clients values (90, "Svedberg","Theodor","theodor.svedberg@email.com","theo",0);
+insert into Clients values (91, "Smalley","Richard","smalley.richard@email.com","smalley",0);
+insert into Clients values (92, "Polanyi","John","polanyi.john@email.com","john",0);
+insert into Clients values (93, "Porter","George","george.porter@email.com","porter",1);
+insert into Clients values (94, "Pauling","Linus","linus.pauling@email.com","lius",0);
+insert into Clients values (95, "Olah","George","george.olah@email.com","olah",0); 
+insert into Clients values (96, "Nernst","Walter","nersnt.walter@email.com","ners",0);
+insert into Clients values (97, "Natta","Giulio","natta.giulio@email.com","natta",0);
+insert into Clients values (98, "Mullis","Kary","kary.mullis@email.com","mullis",0);
+insert into Clients values (99, "Mitchell","Peter","peter.mitchell@email.com","peter",0);
+insert into Clients values (100, "McMillian","Edwin","ed.mcmillian@email.com","ed",0);
+insert into Clients values (101, "Michel","Hartmut","mich.hart@email.com","mich",0);
+insert into Clients values (102, "Karle","Jerome","jerome.karle@email.com","karle",0);
+insert into Clients values (103, "Karlplus","Martin","karl.plus@email.com","k+",0);
+insert into Clients values (104, "Karrer","Paul","karrer.paul@email.com","paul",0);
+insert into Clients values (105, "Kendrew","John","john.kendrew@email.com","ken",0);
+insert into Clients values (106, "Klug","Aaron","klug.aaron@email.com","klug",0);
+insert into Clients values (107, "Kobilka","Brian","kob.brian@email.com","kob",0);
+insert into Clients values (108, "Kohn","Walter","kohn.walter@email.com","kohn",0);
+insert into Clients values (109, "Kornberg","Roger","kornberg.roger@email.com","roger",0);
+insert into Clients values (110, "Kroto","Harold","kroto.harold@email.com","kroto",0);
+insert into Clients values (111, "Kuhn","Richard","richard.kuhn@email.com","kuh",0);                       
+insert into Clients values (112, "Haber","Fritz","fritz.haber@email.com","fr",0);
+insert into Clients values (113, "Hahn","Otto","otto.hahn@email.com","otto",1);
+insert into Clients values (114, "Harden","Odd","harden.odd@email.com","odd",0);
+insert into Clients values (115, "Boltzmann","Ludwig","boltzman.ludwig@email.com","wig",1);
+insert into Clients values (116, "Brunhes","Bernard","bernard.brunches@email.com","nard",1);
+insert into Clients values (117, "Diu","Bernard","bernard.diu@email.com","diu",0);
+insert into Clients values (118, "Graetzel","Michael","michael.graetzel@email.cm","mich",0);
+insert into Clients values (119, "Gonczi","Georges","georges.gonczi@email.com","geo",0);
+insert into Clients values (120, "Bruhat","Georges","georges.bruhat@email.com","bru",0); 
+insert into Clients values (121, "Rocard","Yves","yves.rocard@email.com","yves",0);
+insert into Clients values (122, "Stengers","Isabelle","isa.sten@email.com","sten",1);
+insert into Clients values (123, "Reif","Frederic","fred.reif@email.com","fred",0);
+insert into Clients values (124, "Jancovici","Bernard","nard.jand@email.com","jan",1);  
+insert into Clients values (125, "Kubo","Ryogo","kubo.kubo@email.com","kubo",0);
+insert into Clients values (126, "Barberousse","Anouk","barbe.rousse@email.com","rousse",0);
+insert into Clients values (127, "Brush","Stephen","steph.bruch@email.com","steph",0);
+insert into Clients values (128, "Cercignani","Carlo","carlo.cerc@email.com","carlo",0);
+insert into Clients values (129, "Ehrenfest","Paul","paul.ehrenfest@email.com","paul",0);
+insert into Clients values (130, "Ehrenfest","Tatiana","tatiana.ehren@email.com","tati",0);
+insert into Clients values (131, "Harman","Peter","peter.harman@email.com","peter",0);                     
+insert into Clients values (132, "Maury","Jean-Pierre","jp.maury@email.com","maury",0);
+insert into Clients values (133, "Cork","James","james.cork@email.com","james",0);
+insert into Clients values (134, "Fernandez","Bernard","Fernandez.Bernard@email.com","fer",0);
+insert into Clients values (135, "Valentin","Luc","luc.valentin@email.com","luc",0);
+insert into Clients values (136, "David","Halliday","halliday.david@email.com","haliday",0);
+insert into Clients values (137, "Kaplan","Irving","kaplan.irving@email.com","irving",0);          
+insert into Clients values (138, "Wesley","Addison","addison.weshley@email.com","weshl",0);
+insert into Clients values (139, "Marx","Karl","karl.marx@email.com","karl",0);
+insert into Clients values (140, "Engels","Friedrich","engels.fried@email.com","fried",0);
+insert into Clients values (141, "Joly","Maurice","joly.maurice@email.com","joly",0);
+insert into Clients values (142, "Tucker","Benjamin","Tucker.benjamin@email.com","ben",0);
+insert into Clients values (143, "Brentano","Franz","franz.bren@email.com","franz",0);                                              
+insert into Clients values (145, "Mach","Ernst","ernst.mach@email.com","ernst",0);
+insert into Clients values (146, "James","William","William.james@email.com","james",0);
+insert into Clients values (147, "Cohen","Hermann","cohen.hermann@email.com","herm",0);
+insert into Clients values (148, "Tarde","Gabriel","gabriel.tarde@email.com","gab",0);
+insert into Clients values (149, "Nietzsche","Friedrich","fried.niet@email.com","fried",0);
+insert into Clients values (150, "Frege","Gottlob","frege.gottlob@email.com","frege",0);
+insert into Clients values (151, "Freud","Sigmund","freud.Sigmund@email.com","freud",0);
+insert into Clients values (152, "Hannequin","Arthur","Hanneq.Art@email.com","art",1);
+insert into Clients values (153, "Belot","Gustave","belot.gust@email.com","gust",1);
+insert into Clients values (154, "Dewet","John","john.dewet@email.com","dewet",0);
+insert into Clients values (155, "Bergson","Henri","henri.bergson@email.com","henri",0);
+insert into Clients values (156, "Duhem","Pierre","pierre.duhem@email.com","duhem",0);
+insert into Clients values (157, "Blondel","Maurice","maurice.blondel@email.com","blond",0);
+insert into Clients values (158, "Palante","Georges","georges.palante@email.com","palante",0);
+insert into Clients values (159, "Santayana","George","george.santa@email.com","santa",0);
+insert into Clients values (160, "Weber","Max","weber.max@email.com","max",0);
+insert into Clients values (161, "Chestov","Léon","leon.chestove@email.com","chestov",0):
+insert into Clients values (162, "Croce","Benedetto","croce.benedetto@email.com","croce",0);
+insert into Clients values (163, "Max","Scheler","max.scheler@email.com","max",0);
+insert into Clients values (164, "Maritain","Jacques","maritain.jacques@email.com","jak"0);
+insert into Clients values (165, "Heidegger","Martin","martin.h@email.com","password",0);                      
+insert into Clients values (166, "Dalbiez","Roland","roland.dal@email.com","roland",1);
+insert into Clients values (167, "Carnap","Rudolf","rudol.carnap@email.com","carnap",0);
+insert into Clients values (168, "Hazlitt","Henry","henry.haz@email.com","haz",0);
+insert into Clients values (169, "Marcuse","Herbert","marcuse.herbert@email.com","marc",0);
+insert into Clients values (170, "Strauss","Leo","leo.strauss@email.com","leo",0);                       
+
 
 /* Cinema */
 insert into Cinema values ("Pathé Boulogne", "Pathé Gaumont");
@@ -169,7 +289,6 @@ insert into Participe_au_film values (22, 7);
 insert into Participe_au_film values (23, 7);
 
 /* Se_joue_dans */ /* On va mettre les films la semaine du 16 vu qu on va présenté cette semaine la */
-/* ( num_se_joue, jour, heure, version, num_film, num_salle, nom_du_cinema)*/
 insert into Se_joue_dans values(1, '2019-12-16', '10-00-00', "vf", 1, 1, "Pathé Boulogne");
 insert into Se_joue_dans values(2, '2019-12-16', '10-00-00', "vo", 6, 2, "Pathé Boulogne");
 insert into Se_joue_dans values(3, '2019-12-16', '15-00-00', "vf", 4, 1, "Pathé Boulogne");
@@ -201,44 +320,10 @@ insert into Se_joue_dans values(23, '2019-12-16', '15-00-00', "vf", 7, 1, "UGC V
 insert into Se_joue_dans values(24, '2019-12-16', '17-00-00', "vo", 7, 2, "UGC Versailles");
 
 /* Veut_voir */ /* 6 euro sans reduc, 5 euro avec */
-/* (num_veut_voir, num_se_joue, num_client, num_film, prix) */
-insert into Veut_voir values(1, 1, 1, 1, 6);
-insert into Veut_voir values(2, 1, 1, 1, 6);
-insert into Veut_voir values(3, 1, 2, 1, 5);
-insert into Veut_voir values(4, 1, 50, 1, 6);
-insert into Veut_voir values(5, 1, 50, 1, 6);
-insert into Veut_voir values(6, 1, 50, 1, 6);
-insert into Veut_voir values(7, 1, 50, 1, 6);
-insert into Veut_voir values(8, 1, 27, 1, 6);
-insert into Veut_voir values(9, 1, 27, 1, 6);
-insert into Veut_voir values(10, 1, 16, 1, 5);
-insert into Veut_voir values(11, 1, 16, 1, 5);
-
-insert into Veut_voir values(12, 2, 2, 6, 5);
-insert into Veut_voir values(13, 2, 2, 6, 5);
-insert into Veut_voir values(14, 2, 45, 6, 6);
-insert into Veut_voir values(15, 2, 34, 6, 5);
-insert into Veut_voir values(16, 2, 34, 6, 5);
-insert into Veut_voir values(17, 2, 34, 6, 5);
-insert into Veut_voir values(18, 2, 34, 6, 5);
-insert into Veut_voir values(19, 2, 34, 6, 5);
-insert into Veut_voir values(20, 2, 34, 6, 5);
-insert into Veut_voir values(21, 2, 34, 6, 5);
-insert into Veut_voir values(22, 2, 7, 6, 6);
-insert into Veut_voir values(23, 2, 7, 6, 6);
-insert into Veut_voir values(24, 2, 7, 6, 6);
-insert into Veut_voir values(25, 2, 7, 6, 6);
-insert into Veut_voir values(26, 2, 7, 6, 6);
-
-insert into Veut_voir values(27, 3, 10, 4, 6);
-insert into Veut_voir values(28, 3, 10, 4, 6);
-insert into Veut_voir values(29, 3, 10, 4, 5);
-insert into Veut_voir values(30, 3, 11, 4, 6);
-insert into Veut_voir values(31, 3, 11, 4, 6);
-insert into Veut_voir values(32, 3, 49, 4, 6);
-insert into Veut_voir values(33, 3, 1, 4, 6);
-insert into Veut_voir values(34, 3, 17, 4, 6);
-insert into Veut_voir values(35, 3, 37, 4, 6);
+insert into Veut_voir values(1, 1, 1, 6);
+insert into Veut_voir values(2, 1, 1, 6);
+insert into Veut_voir values(3, 2, 1, 5);
+insert into Veut_voir values(4, 50, 1, 6);
 
 /* Note */
 insert into Note values(21,4,4);
@@ -255,3 +340,10 @@ insert into Note values(6,3,5);
 insert into Note values(12,2,2);
 insert into Note values(34,1,3);
 insert into Note values(1,1,3);
+
+/* Ticket */ /* Quand un client achete une place il y a un uplet qui s'ajoute dans cette tavle aussi */
+insert into Ticket values(1, 1, 1, 1);
+insert into Ticket values(2, 1, 1, 1);
+insert into Ticket values(3, 1, 2, 1);
+insert into Ticket values(4, 1, 50, 1);
+
