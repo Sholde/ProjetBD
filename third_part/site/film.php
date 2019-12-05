cf921a5f2db294fd4a2cefa830179e70e9bd4071
taariq <taariq.dedarally@ens.uvsq.fr>
2019-12-05 22:25:18 +0100

    Ajout de la note dans la description d'un film

--- third_part/site/film.php
+++ third_part/site/film.php
@@ -10,8 +10,14 @@
 	
 	$link->select_db('Projet') or die("Erreur de selection de la BD: " . $link->error);
 	
-	$query = "Select f.num_film, f.nom, f.genre, f.duree, f.origine,f.version_disponible from Film f where f.num_film = $film;";
-	$result = $link->query($query) or die("not exist in data base ");
+	$query  = "SELECT f.nom, avg(n.note) AS moyenne,COUNT(n.num_client) AS nb_note,f.origine,f.duree,f.version_disponible,f.genre FROM Note n,Film f WHERE f.num_film = $film and n.num_film = $film;";
+	
+	$query2 = "select f_prec.nom as nom_film1, f_suiv.nom as nom_film2
+						    from Film f_prec, Film f_suiv, Suit s
+						    where f_prec.num_film = s.num_film_prec and f_suiv.num_film = s.num_film_suiv;";
+	
+	$film_liee = $link->query($query2) or die("not exist in data base");		    
+	$result    = $link->query($query) or die("not exist in data base ");
 	
 	print "<table>";
 	while ($tuple = mysqli_fetch_object($result)){ 
@@ -20,7 +26,16 @@
 						</tr>
 						<tr>
 						<td>genre: $tuple->genre - origine: $tuple->origine - duree: $tuple->duree - version: $tuple->version_disponible</td>
+						</tr>
+						<tr>
+						<td>Note du film:$tuple->moyenne - Nombre de votes: $tuple->nb_note</td>
 						</tr>";
+	while ($tuple2 = mysqli_fetch_object($film_liee)){ 
+		print " <tr>
+						<td> $tuple2->nom_film1 </td>
+						<td> $tuple2->nom_film2 </td>
+						</tr>";				
+	}
 	}
 	print "</table>";
 	
