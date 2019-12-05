2b2c4c5bab87dcdfc06ef2211e0ba89acdf97f78
Sholde <exshode@gmail.com>
2019-12-05 21:30:28 +0100

    avancement de la page de reservation

--- third_part/site/formulaire_reserve.php
+++ third_part/site/formulaire_reserve.php
@@ -16,6 +16,7 @@
 	/* variable */
 	$num_se_joue = $_GET['num_se_joue'];
 	$num_film = $_GET['num_film'];
+	$email_client = $_SESSION['session'];
 	
 	/* connexion serveur */
 	$link = new mysqli("localhost", "Client", "client");
@@ -26,19 +27,64 @@
 	/* connexion bd */
 	$link->select_db('Projet') or die("Erreur de selection de la BD: " . $link->error);
 	
-	$query_se_joue = "Select * from Se_joue_dans where num_se_joue = $num_se_joue;";
-	$num_se_joue = $link->query($query_se_joue) or die("erreur select");
+	$query_se_joue = "Select * from Se_joue_dans J, Salle S where S.num_salle = J.num_salle and S.nom_du_cinema = J.nom_du_cinema and J.num_se_joue = $num_se_joue;";
+	$se_joue = $link->query($query_se_joue) or die("erreur select");
 	
-	$tuple = mysqli_fetch_object($num_se_joue);
+	$query_film = "Select * from Film where num_film = $num_film;";
+	$film = $link->query($query_film) or die("erreur select");
+	
+	$query_client = "Select * from Clients where email = '$email_client';";
+	$client = $link->query($query_client) or die("erreur select");
 	
 	print "<form method=\"POST\" action=\"reserve\">";
 	print "<table>";
+	$tuple = mysqli_fetch_object($film);
 	print "
 		<tr>
 		<td>Nom du film : </td>
-		<td>$tuple->num_film</td>
+		<td>$tuple->nom</td>
 		</tr>
 	";
+	$tuple = mysqli_fetch_object($se_joue);
+	print "
+		<tr>
+		<td>Cinéma : </td>
+		<td>$tuple->nom_du_cinema</td>
+		</tr>
+		<tr>
+		<td>Ville : </td>
+		<td>$tuple->ville</td>
+		</tr>
+		<tr>
+		<td>Salle : </td>
+		<td>$tuple->num_salle</td>
+		</tr>
+		<tr>
+		<td>Jour : </td>
+		<td>$tuple->jour</td>
+		</tr>
+		<tr>
+		<td>heure : </td>
+		<td>$tuple->heure</td>
+		</tr>
+	";
+	$tuple = mysqli_fetch_object($client);
+	if($tuple->reduction) {
+		print "
+			<tr>
+			<td>Prix : </td>
+			<td>5</td>
+			</tr>
+		";
+		}
+	else {
+		print "
+			<tr>
+			<td>Prix : </td>
+			<td>6</td>
+			</tr>
+		";
+	}
 	print "</table>";
 	print "</form>";
 	
