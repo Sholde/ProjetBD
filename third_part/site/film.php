18d279ebf8c34856cf7f2b931135da5fc060658b
Sholde <exshode@gmail.com>
2019-12-05 23:13:22 +0100

    correction de tes problèmes

--- third_part/site/film.php
+++ third_part/site/film.php
@@ -2,7 +2,7 @@
 	
 	print "<html><head><title>Film</title></head><body>";
 	
-	$film = $_GET['num_film'];
+	$num_film = $_GET['num_film'];
 	
 	$link = new mysqli("localhost", "Anonyme", "anonyme");
 	if($link->connect_errno) {
@@ -11,14 +11,26 @@
 	
 	$link->select_db('Projet') or die("Erreur de selection de la BD: " . $link->error);
 	
-	$query  = "SELECT f.nom, avg(n.note) AS moyenne,COUNT(n.num_client) AS nb_note,f.origine,f.duree,f.version_disponible,f.genre FROM Note n,Film f WHERE f.num_film = $film and n.num_film = $film;";
+	$query = "SELECT F.*, avg(N.note) AS moyenne, COUNT(N.num_client) AS nb_note FROM Note N, Film F WHERE F.num_film = $num_film and N.num_film = $num_film;";
+	$result = $link->query($query) or die("not exist in data base");
 	
-	$query2 = "select f_prec.nom as nom_film1, f_suiv.nom as nom_film2
-						    from Film f_prec, Film f_suiv, Suit s
-						    where f_prec.num_film = s.num_film_prec and f_suiv.num_film = s.num_film_suiv;";
+	$query_suiv = "
+		select F_suiv.num_film as num, F_suiv.nom as nom
+		from Film F_prec, Film F_suiv, Suit S
+		where F_prec.num_film = S.num_film_prec
+		and F_suiv.num_film = S.num_film_suiv
+		and S.num_film_prec = $num_film;
+	";
+	$film_suiv = $link->query($query_suiv) or die("not exist in data base 2");
 	
-	$film_liee = $link->query($query2) or die("not exist in data base");		    
-	$result    = $link->query($query) or die("not exist in data base ");
+	$query_prec = "
+		select F_prec.num_film as num, F_prec.nom as nom
+		from Film F_prec, Film F_suiv, Suit S
+		where F_prec.num_film = S.num_film_prec
+		and F_suiv.num_film = S.num_film_suiv
+		and S.num_film_suiv = $num_film;
+	";
+	$film_prec = $link->query($query_prec) or die("not exist in data base 2");
 	
 	print "<table>";
 	while ($tuple = mysqli_fetch_object($result)){ 
@@ -26,17 +38,28 @@
 						<td> $tuple->nom </td>
 						</tr>
 						<tr>
-						<td>genre: $tuple->genre - origine: $tuple->origine - duree: $tuple->duree - version: $tuple->version_disponible</td>
+						<td>genre: $tuple->genre  -  origine: $tuple->origine  -  duree: $tuple->duree  -  version: $tuple->version_disponible</td>
 						</tr>
 						<tr>
 						<td>Note du film:$tuple->moyenne - Nombre de votes: $tuple->nb_note</td>
-						</tr>";
-	while ($tuple2 = mysqli_fetch_object($film_liee)){ 
-		print " <tr>
-						<td> $tuple2->nom_film1 </td>
-						<td> $tuple2->nom_film2 </td>
-						</tr>";				
-	}
+						</tr>
+		";
+		$prec = mysqli_fetch_object($film_prec);
+		if($prec) {
+			print "
+				<tr>
+				<td>Film précédent : <a href=\"film.php?num_film=$prec->num\">$prec->nom</a></td>
+				</tr>
+			";
+		}
+		$suiv = mysqli_fetch_object($film_suiv);
+		if($suiv) {
+			print "
+				<tr>
+				<td>Film suivant : <a href=\"film.php?num_film=$suiv->num\">$suiv->nom</a></td>
+				</tr>
+			";
+		}
 	}
 	print "</table>";
 	
--- third_part/site/liste_film.php
+++ third_part/site/liste_film.php
@@ -8,14 +8,14 @@
 	
 	$link->select_db('Projet') or die("Erreur de selection de la BD: " . $link->error);
 	
-	$query = "Select f.num_film, f.nom, f.genre from Film f;";
+	$query = "Select F.num_film, F.nom, F.genre from Film F;";
 	$result = $link->query($query) or die("erreur select");
 	
 	print "<table>";
 	while ($tuple = mysqli_fetch_object($result)){ 
 		print "
 			<tr>
-			<td><a href=\"film.php?num=$tuple->num_film\">$tuple->nom</a></td>
+			<td><a href=\"film.php?num_film=$tuple->num_film\">$tuple->nom</a></td>
 			<td><a href=\"projection_film.php?num_film=$tuple->num_film\">Voir les projection</td>
 			</tr>";
 	}
