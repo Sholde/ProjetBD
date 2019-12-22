af8893c5b986c89a681914a6ae2783c743aa61d1
Sholde <exshode@gmail.com>
2019-12-22 23:24:21 +0100

    rangement

--- third_part/BD/source/alter_table.sql
+++
@@ -1,3 +0,0 @@
-/* rajouter un commentaire a la note d'un film */
-alter table Note
-add commentaire varchar(256);
--- third_part/site_admin/modifier_note.php
+++
@@ -1,5 +0,0 @@
-<html>
-	<body>
-		est-ce que l'admin peut modif la note ?
-	</body>
-</html>
